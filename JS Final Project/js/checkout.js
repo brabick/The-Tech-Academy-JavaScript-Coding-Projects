fcc06b8dd8b83e318aaa05a8ecf9b7fa8116de2c
Brandon <brabick@gmail.com>
2019-02-06 11:17:28 -0700

    Optimize code for checkout function

--- JS Final Project/js/checkout.js
+++ JS Final Project/js/checkout.js
@@ -5,12 +5,13 @@
         var sizeTotal = 0;
         var sizeArray = document.getElementsByClassName("size");
       
-  
+      //Add sizes to array and assigns selected size to variable
+
       for (i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
           var selectedSize = sizeArray[i].value;
         };
-        if (selectedSize === "Small Pizza") {
+       /* if (selectedSize === "Small Pizza") {
           text1 = text1 + selectedSize + " $6.00" + "<br>";
         } else if (selectedSize === "Medium Pizza") {
           text1 = text1 + selectedSize + " $10.00" + "<br>";
@@ -18,19 +19,27 @@
           text1 = text1 + selectedSize + " $14.00" + "<br>";
         } else if (selectedSize === "Colossal Pizza") {
           text1 = text1 + selectedSize + " $16.00" + "<br>";
+          
         };
+        consolidated two if statements
+        */
  
-        
+        // Check selected size, assigns cost and prints the selection
         if (selectedSize === "Small Pizza") {
           sizeTotal = 6;
+          text1 = text1 + selectedSize + " $" + sizeTotal + "<br>";
         } else if (selectedSize === "Medium Pizza") {
           sizeTotal = 10;
+          text1 = text1 + selectedSize + " $" + sizeTotal + "<br>";
         } else if (selectedSize === "Large Pizza") {
           sizeTotal = 14;
+          text1 = text1 + selectedSize + " $" + sizeTotal + "<br>";
         } else if (selectedSize === "Colossal Pizza") {
           sizeTotal = 16;
+          text1 = text1 + selectedSize + " $" + sizeTotal + "<br>";
         }
         
+        //Prints running total and calls the next function in the series
         runningTotal = sizeTotal;
         getMeat(runningTotal, text1);
         
@@ -48,17 +57,22 @@
         var meatTotal= 0;
         var selectedMeat = [];
         var meatArray = document.getElementsByClassName("meat");
-  
+
+        //Add all meats to array, check the meats that are selected and push those selected meats to another array
+
         for (var j = 0; j < meatArray.length; j++) {
           if (meatArray[j].checked) {
             selectedMeat.push(meatArray[j].value);
-            if (meatArray[j].value == meatArray[0].value) {
+           if (meatArray[j].value == meatArray[0].value) {
             text1 = text1 + meatArray[j].value + " $0.00" + "<br>";
             } else {
-              text1= text1 + meatArray[j].value + " $1.00" + "<br>";
-            };
+              text1 = text1 + meatArray[j].value + " $1.00" + "<br>";
+            }; 
           };
-        };
+        };  
+          
+
+          //Count the number of meat options selected and prints the amount each meat is adding to the total
         var meatCount = selectedMeat.length;
         if (meatCount > 1) {
           meatTotal = (meatCount - 1)
@@ -66,13 +80,14 @@
           meatTotal = 0;
         };
         
+        //Prints running total and calls the next function in the series
         runningTotal = (runningTotal + meatTotal);
         document.getElementById("text").innerHTML = text1;
         document.getElementById("total").innerHTML = "<h3>Total: $"+ runningTotal + ".00" + "</h3>";
         getVeggie(runningTotal, text1);
-        
-  
+
       };
+   
   
       //End Meat Function
 
@@ -82,18 +97,20 @@
         var veggieTotal = 0;
         var selectedVeggie = [];
         var veggieArray = document.getElementsByClassName("veggie");
-  
+        //Add all veggies to array, check the veggies that are selected and push those selected veggies to another array
         for (var j = 0; j < veggieArray.length; j++) {
           if (veggieArray[j].checked) {
             selectedVeggie.push(veggieArray[j].value);
-            if (veggieArray[j].value == veggieArray[0].value) {
+           if (veggieArray[j].value == veggieArray[0].value) {
               text1 = text1 + veggieArray[j].value + " $0.00" + "<br>";
             } else {
               text1 = text1 + veggieArray[j].value + " $1.00" + "<br>";
-            }
+            } 
+          };
             
           };
-        };
+        
+        
         var veggieCount = selectedVeggie.length;
         if (veggieCount > 1) {
           veggieTotal = (veggieCount - 1)
@@ -101,11 +118,12 @@
           veggieTotal = 0;
         };
           runningTotal = (veggieTotal + runningTotal);
-    
+        //Prints running total and calls the next function in the series
           document.getElementById("text").innerHTML = text1;
           document.getElementById("total").innerHTML = "<h3>Total: $"+ runningTotal + ".00" + "</h3>";
           getCrust(runningTotal, text1);
       };
+      
 
       //End Veggie Function
 
@@ -132,7 +150,7 @@
           crustTotal = 0;
         }
         };
-  
+        //Prints running total and calls the next function in the series
         runningTotal = (runningTotal + crustTotal);
         document.getElementById("text").innerHTML = text1;
         document.getElementById("total").innerHTML = "<h3>Total: $" + runningTotal + ".00" + "</h3>";
@@ -162,7 +180,7 @@
           cheeseTotal = 0;
         }
         };
-  
+        //Prints running total and calls the next function in the series
         runningTotal = (runningTotal + cheeseTotal);
         document.getElementById("text").innerHTML = text1;
         document.getElementById("total").innerHTML = "<h3>Total: $" + runningTotal + ".00" + "</h3>";
@@ -182,6 +200,7 @@
           var selectedSauce = sauceArray[l].value;
           text1 = text1 + selectedSauce + " $0.00" + "<br>";
         };
+        //Prints running total
         document.getElementById("text").innerHTML = text1;
        };
       };
